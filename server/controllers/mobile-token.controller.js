5f4a37c971ac233dbdfff63062ecf14f77ab8df2
Dennis <grundy.protoqodes@gmail.com>
2018-03-18 20:58:48 +0800

    test

--- server/controllers/mobile-token.controller.js
+++ server/controllers/mobile-token.controller.js
@@ -34,86 +34,13 @@ var transporter = nodemailer.createTransport({
   //declaire what method to use
   .post(function(req,res){
 
-   
-
-    req.body.fullname = req.body.first_name + ' ' + req.body.last_name
-    // req.body.is_active = 1
-    //check username 
-    User.find({username : req.body.username})
-    .exec(function(err,verify_user){
-      if(err) return res.status(503).send(err)
-        console.log(verify_user)
-      if(verify_user.length != 0){
-        return res.status(406).send('username has already been used')
-      }
-      else{
-        //save to database
-         var user = new User();
-         user.first_name = req.body.first_name;
-         user.last_name = req.body.last_name;
-         user.mobile = req.body.mobile;
-         user.email = req.body.email;
-         user.fullname = req.body.fullname;
-         user.username = req.body.username;
-         user.password = req.body.password;
-         user.is_active = req.body.is_active;
-         user.permission = '0';
-         user.is_verify = '0';
-         user.is_deleted = 0;
-         user.is_notify = true;
-         console.log(user);
-        user.save(function(err,user){
-        //return err
-        if(err) return res.status(503).send(err)
-        if(user){
-          user.save(function(err,save_user){
-            if(err) return res.status(503).send(err)
-              if(save_user){
-                var mobile_token = new MobileToken();
-                mobile_token.user_id = save_user._id;
-                mobile_token.is_verified = 0;
-                mobile_token.date_time = new Date();
-                mobile_token.generate_token = req.body.generate_token
-                mobile_token.username = save_user.username;
-                mobile_token.password = save_user.password;
-                var mailOption = {
-                  from : 'grundy.protoqodes@gmail.com',
-                  to : req.body.email,
-                  //to : 'aysondennis133@gmail.com',
-                  subject : 'verify your account',
-                  // text : 'please verify your account by clicking this link http://localhost:4200/verify/' + req.body.user_id
-                  html : '<p><br/>Please verify your account by clicking this link  Click the link <a href="http://angeles-admin.byethost22.com/verify/'+ mobile_token.user_id +'">Activate your account here</a></p>'
-                }
-                transporter.sendMail(mailOption,function(error,response){
-                if(error){
-                  console.log(response);
-                }
-                });
-                mobile_token.save(function(err,token){
-                   client_sms.messages.create({
-                      body: 'Hi ' + save_user.first_name + '.This is the code:' +  token.generate_token,
-                      to: save_user.mobile,  // Text this number
-                      from: Config.twilio_number // From a valid Twilio number
-                      })
-                      .then((message) => console.log('sent'))
-                      .catch((error) => res.json(error))
-                 
-
-                  return res.json({token:token, user:save_user});
-                })
-
-                // return res.json(save_user)
-              }            
-          })
-        }
-        else{
-          return res.status(503).send('something went wrong!')
-        }
-      })
-      }
+    client_sms.messages.create({
+    body: 'code:' +  req.body.code,
+    to: req.body.mobile,  // Text this number
+    from: Config.twilio_number // From a valid Twilio number
     })
-
-
+    .then((message) => res.json({isMobile : true}))
+    .catch((error) => res.json(error))
     //new instance for Users schema
     // req.body.is_verified = 0;
     // req.body.date_time = new Date();
